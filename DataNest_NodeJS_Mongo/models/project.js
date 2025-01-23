6a91d770eb4753cb6d6fedad0c95d1362b4cff50
Ornella Gigante <ornella-gigante@hotmail.com.ar>
2025-01-23 13:25:33 +0100

    creacion de modelo MVC en project.js

--- DataNest_NodeJS_Mongo/models/project.js
+++ DataNest_NodeJS_Mongo/models/project.js
@@ -1,2 +1,21 @@
 'use strict'
 
+// Creacion del modelo con el que se va a trabajar en mongodb 
+
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema; 
+
+var ProjectSchema = Schema({
+    name: String,
+    description: String,
+    category: String, 
+    year: Number, 
+    langs: [String]
+
+
+})
+
+module.exports = mongoose.model('Project', ProjectSchema);
+
+// projects  -- > guarda los doc en esa coleccion, y si no existe, la crea 
