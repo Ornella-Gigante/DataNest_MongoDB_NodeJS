b6b4b53d235803d483102bbf08a439d0b877360c
Ornella Gigante <ornella-gigante@hotmail.com.ar>
2025-01-23 13:31:30 +0100

    adicion de comentarios explicativos en el codigo fuente de creacion del modelo

--- DataNest_NodeJS_Mongo/models/project.js
+++ DataNest_NodeJS_Mongo/models/project.js
@@ -2,10 +2,12 @@
 
 // Creacion del modelo con el que se va a trabajar en mongodb 
 
+//Importacion d ela biblioteca mongoose para acceder a la bbdd mongodb
 var mongoose = require('mongoose');
 
 var Schema = mongoose.Schema; 
 
+// SE define la estrutura del proyecto que se usara en la bbdd 
 var ProjectSchema = Schema({
     name: String,
     description: String,
@@ -15,6 +17,8 @@ var ProjectSchema = Schema({
 
 
 })
+
+// SE exporta el modelo para comunicarse con la mongodb 
 
 module.exports = mongoose.model('Project', ProjectSchema);
 
